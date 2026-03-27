438e816a0ec932ca7083e59350216d2ac3dd4cd0
nguyenxuanson01012007-ship-it <nguyenxuanson01012007@gmail.com>
2026-03-27 12:09:31 +0700

    Add files via upload

---
+++ b2/main.js
@@ -0,0 +1,54 @@
+let queue = [];
+let isShowing = false;
+
+function createToast(type) {
+  const messages = {
+    success: "Thành công!",
+    error: "Có lỗi xảy ra!",
+    info: "Thông tin!",
+    warning: "Cảnh báo!",
+  };
+
+  queue.push({ type, message: messages[type] });
+  showNext();
+}
+
+function showNext() {
+  if (isShowing) return;
+  if (queue.length === 0) return;
+
+  isShowing = true;
+
+  const { type, message } = queue.shift();
+
+  const container = document.getElementById("toast-container");
+
+  const toast = document.createElement("div");
+  toast.className = "toast " + type;
+
+  toast.innerHTML = `
+    <span>${message}</span>
+    <span class="close">×</span>
+    <div class="progress"></div>
+  `;
+
+  container.appendChild(toast);
+
+  const progress = toast.querySelector(".progress");
+  progress.style.animation = "progress 4s linear forwards";
+
+  const removeToast = () => {
+    toast.style.animation = "slideOut 0.4s forwards";
+    setTimeout(() => {
+      toast.remove();
+      isShowing = false;
+      showNext();
+    }, 400);
+  };
+
+  // auto remove
+  setTimeout(removeToast, 4000);
+
+  // click close
+  toast.querySelector(".close").onclick = removeToast;
+}
